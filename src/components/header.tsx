7bb86e8d9d25006c033bda3bb60d408bb23b9896
RetrasTT <retrast.keke3h@gmail.com>
2024-03-09 19:44:14 +0700

    change header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,17 +3,25 @@ import { homePagePath, authPagePath, settingsPagePath } from "../lib/paths";
 import { Button } from "primereact/button";
 import { cn } from "../lib/utils";
 import { user } from "../lib/placeholders";
+import { Avatar } from "primereact/avatar";
 
 export default function Header() {
   return (
     <header className="mx-4">
       <div className="container mx-auto mt-4 flex h-[46px] items-center justify-between rounded bg-[var(--primary-color)] ">
         <Link to={homePagePath} className="text-sm font-semibold tracking-wide text-[var(--primary-color-text)]">
-          ChatApp - {user.username}
+          {user.isAuth ? (
+            <div className="flex items-center gap-x-2">
+              <Avatar id="avatar" icon="pi pi-user" className="h-[28px] w-[28px] text-[var(--text-color)]" />
+              <p>Retrast</p>
+            </div>
+          ) : (
+            "ChatApp"
+          )}
         </Link>
 
         <nav>
-          <ul className="flex gap-x-4">
+          <ul className="flex gap-x-1">
             <li className={cn({ hidden: !user.isAuth })}>
               <Link to={homePagePath} className="font-medium text-[var(--highlight-text-color)]">
                 <Button icon="pi pi-comments" size="small" className="h-[28px] w-[28px] p-0" />
@@ -28,7 +36,7 @@ export default function Header() {
 
             <li className={cn({ hidden: !user.isAuth })}>
               <Link to={"#"} className="font-medium text-[var(--highlight-text-color)]">
-                <Button icon="pi pi-sign-out" size="small" className="h-[28px] w-[28px] p-0" />
+                <Button icon="pi pi-sign-out" label="Sign out" size="small" className="h-[28px] p-0 px-2" />
               </Link>
             </li>
 
